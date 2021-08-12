1b5dda704dfd802d2c2aa808392a8b0b31528ee1
arryan <arryan@umich.edu>
2021-08-12 15:27:53 -0400

    Updates

---
+++ precache-manifest.9fc6f64dc8dbbe223c6f830491c8d597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "602c9061607840a25179",
+    "url": "/lights_out/static/css/main.c4db940f.chunk.css"
+  },
+  {
+    "revision": "602c9061607840a25179",
+    "url": "/lights_out/static/js/main.602c9061.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lights_out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "9f8678a28ec394cd6ac8",
+    "url": "/lights_out/static/js/runtime~main.9f8678a2.js"
+  },
+  {
+    "revision": "8c5c5bfaeb8cb344bf31f3c958f65883",
+    "url": "/lights_out/index.html"
+  }
+];
